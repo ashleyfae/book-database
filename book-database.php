613a9998a83c2366fdcd2055b8835626c05a963d
Ashley Evans <cazzyae@gmail.com>
2016-10-30 18:45:29 +0000

    Admin actions.

--- book-database.php
+++ book-database.php
@@ -227,6 +227,7 @@ if ( ! class_exists( 'Book_Database' ) ) :
 			require_once BDB_DIR . 'includes/review-functions.php';
 
 			if ( is_admin() ) {
+				require_once BDB_DIR . 'includes/admin/admin-actions.php';
 				require_once BDB_DIR . 'includes/admin/admin-pages.php';
 				require_once BDB_DIR . 'includes/admin/admin-assets.php';
 				require_once BDB_DIR . 'includes/admin/books/book-actions.php';
---
+++ includes/admin/admin-actions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Actions
+ *
+ * @package   book-database
+ * @copyright Copyright (c) 2016, Ashley Gibson
+ * @license   GPL2+
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Processes all BDB actions sent via POST and GET by looking for the 'bdb-action'
+ * request and running do_action() to call the function
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_process_actions() {
+	if ( isset( $_POST['bdb-action'] ) ) {
+		do_action( 'book-database/' . $_POST['bdb-action'], $_POST );
+	}
+
+	if ( isset( $_GET['bdb-action'] ) ) {
+		do_action( 'book-database/' . $_GET['bdb-action'], $_GET );
+	}
+}
+
+add_action( 'admin_init', 'bdb_process_actions' );

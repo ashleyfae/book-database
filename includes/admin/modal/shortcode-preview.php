f2415fc429754368453986d5e8de11607b207fdc
Ashley Evans <cazzyae@gmail.com>
2016-10-31 14:17:52 +0000

    Made ajax preview actually do something useful.

--- includes/admin/modal/shortcode-preview.php
+++ includes/admin/modal/shortcode-preview.php
@@ -15,11 +15,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Render Shortcode Preview
  *
+ * @todo  Turn this into real book preview.
+ *
  * @since 1.0.0
  * @return void
  */
 function bdb_tinymce_shortcode_preview() {
-	wp_send_json_success('hi');
+	check_ajax_referer( 'book-database', 'nonce' );
+
+	$book_id = isset( $_POST['book_id'] ) ? absint( $_POST['book_id'] ) : 0;
+	$book    = new BDB_Book( $book_id );
+
+	$template = '[title] [author]';
+
+	$title  = $book->get_title() ? '<strong>' . $book->get_title() . '</strong>' : '';
+	$author = $book->get_author_names() ? sprintf( __( ' by %s', 'book-database' ), $book->get_author_names() ) : '';
+
+	$find = array(
+		'[title]',
+		'[author]'
+	);
+
+	$replace = array( $title, $author );
+
+	wp_send_json_success( str_replace( $find, $replace, $template ) );
 }
 
 add_action( 'wp_ajax_bdb_shortcode_preview', 'bdb_tinymce_shortcode_preview' );
@@ -33,7 +52,7 @@ add_action( 'wp_ajax_bdb_shortcode_preview', 'bdb_tinymce_shortcode_preview' );
  * @return array
  */
 function bdb_tinymce_shortcode_plugin( $plugin_array ) {
-	$suffix                        = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	$suffix                       = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 	$plugin_array['bookdatabase'] = BDB_URL . 'assets/js/admin/shortcode-tinymce' . $suffix . '.js';
 
 	return $plugin_array;

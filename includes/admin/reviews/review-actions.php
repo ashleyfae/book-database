64477ed172c3dc086ab47ba6e73df7b706a442b3
Ashley Evans <cazzyae@gmail.com>
2016-11-09 15:02:41 +0000

    Built out insert review function.

--- includes/admin/reviews/review-actions.php
+++ includes/admin/reviews/review-actions.php
@@ -15,3 +15,55 @@ if ( ! defined( 'ABSPATH' ) ) {
 /*
  * Below: Review Fields
  */
+
+/*
+ * Below: Saving Functions
+ */
+
+/**
+ * Save Review
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_save_review() {
+
+	$nonce = isset( $_POST['bdb_save_review_nonce'] ) ? $_POST['bdb_save_review_nonce'] : false;
+
+	if ( ! $nonce ) {
+		return;
+	}
+
+	if ( ! wp_verify_nonce( $nonce, 'bdb_save_review' ) ) {
+		wp_die( __( 'Failed security check.', 'book-database' ) );
+	}
+
+	if ( ! current_user_can( 'edit_posts' ) ) { // @todo maybe change
+		wp_die( __( 'You don\'t have permission to edit reviews.', 'book-database' ) );
+	}
+
+	$review_id = absint( $_POST['review_id'] );
+
+	$review_data = array(
+		'ID' => $review_id
+	);
+
+	// @todo
+
+	$new_review_id = bdb_insert_review( $review_data );
+
+	if ( ! $new_review_id || is_wp_error( $new_review_id ) ) {
+		wp_die( __( 'An error occurred while inserting the review.', 'book-database' ) );
+	}
+
+	$edit_url = add_query_arg( array(
+		'update-success' => 'true'
+	), bdb_get_admin_page_edit_review( absint( $new_review_id ) ) );
+
+	wp_safe_redirect( $edit_url );
+
+	exit;
+
+}
+
+add_action( 'book-database/review/save', 'bdb_save_review' );
--- includes/review-functions.php
+++ includes/review-functions.php
@@ -44,3 +44,77 @@ function bdb_get_post_reviews( $post_id = 0 ) {
 function bdb_count_total_reviews( $args ) {
 	return book_database()->reviews->count( $args );
 }
+
+/**
+ * Insert New Review
+ *
+ * If the `ID` key is passed into the `$data` array then an existing review
+ * is updated instead.
+ *
+ * @param array $data   Review data. Arguments include:
+ *                      `ID` - To update an existing book (optional).
+ *                      `book_id` - Book ID associated with the review (optional).
+ *                      `post_id` - Post associated with the review (optional).
+ *                      `url` - URL to an external book review (optional).
+ *                      `user_id` - User who the review is for. If omitted, current user is used.
+ *                      `rating` - Rating for the book (optional).
+ *                      `date_added` - Timestamp for when the review was added. If omitted, current time is used.
+ *
+ * @since 1.0.0
+ * @return int|WP_Error ID of the review inserted or updated, or WP_Error on failure.
+ */
+function bdb_insert_review( $data = array() ) {
+
+	$review_db_data = array();
+
+	// Book ID
+	if ( array_key_exists( 'book_id', $data ) && $data['book_id'] > 0 ) {
+		$review_db_data['book_id'] = absint( $data['book_id'] );
+	}
+
+	// Post ID
+	if ( array_key_exists( 'post_id', $data ) && $data['post_id'] > 0 ) {
+		$review_db_data['post_id'] = absint( $data['post_id'] );
+	}
+
+	// URL
+	if ( array_key_exists( 'url', $data ) ) {
+		$review_db_data['url'] = esc_url_raw( $data['url'] );
+	}
+
+	// User ID
+	if ( array_key_exists( 'user_id', $data ) && $data['user_id'] >= 0 ) {
+		$review_db_data['user_id'] = absint( $data['user_id'] );
+	} else {
+		$current_user              = wp_get_current_user();
+		$review_db_data['user_id'] = absint( $current_user->ID );
+	}
+
+	// Rating
+	if ( array_key_exists( 'rating', $data ) ) {
+		$allowed_ratings = bdb_get_available_ratings();
+
+		if ( array_key_exists( $data['rating'], $allowed_ratings ) ) {
+			$review_db_data['rating'] = sanitize_text_field( $data['rating'] );
+		}
+	}
+
+	// Date Added
+	if ( array_key_exists( 'date_added', $data ) ) {
+		$review_db_data['date_added'] = sanitize_text_field( $data['date_added'] );
+	}
+
+	// Review ID
+	if ( array_key_exists( 'ID', $data ) && $data['ID'] > 0 ) {
+		$review_db_data['ID'] = absint( $data['ID'] );
+	}
+
+	$review_id = book_database()->reviews->add( $review_db_data );
+
+	if ( ! $review_id ) {
+		return new WP_Error( 'error-inserting-review', __( 'Error inserting review into database.', 'book-database' ) );
+	}
+
+	return $review_id;
+
+}

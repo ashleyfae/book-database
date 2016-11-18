3afebd57d3e256e04c2635396de84db6dfc5ca87
Ashley Evans <cazzyae@gmail.com>
2016-11-18 15:19:15 +0000

    Migrated book review filter form to actions.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -105,103 +105,10 @@ function bdb_book_reviews_shortcode( $atts, $content = '' ) {
 	$query->query();
 	$template = bdb_get_template_part( 'shortcode-book-reviews-entry', '', false );
 
-	$author_name    = $vars['author_name'];
-	$current_rating = $vars['rating'] ? $vars['rating'] : 'any';
-
-	// Special circumstances to make author name show up in text box.
-	if ( isset( $vars['terms']['author'] ) ) {
-		$author = bdb_get_term( array(
-			'term_id' => absint( $vars['terms']['author'] ),
-			'fields'  => 'names'
-		) );
-
-		if ( $author ) {
-			$author_name = $author;
-		}
-	}
-
-	$review_years = bdb_get_review_years();
-
 	ob_start();
 	?>
 	<form id="bookdb-filter-book-reviews" action="<?php echo esc_url( get_permalink() ); ?>" method="GET">
-		<p class="bookdb-filter-option">
-			<label for="bookdb-book-title"><?php _e( 'Book Title', 'book-database' ); ?></label>
-			<input type="text" id="bookdb-book-title" name="title" value="<?php echo esc_attr( wp_strip_all_tags( $vars['book_title'] ) ); ?>">
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-book-author"><?php _e( 'Author', 'book-database' ); ?></label>
-			<input type="text" id="bookdb-book-author" name="author" value="<?php echo esc_attr( wp_strip_all_tags( $author_name ) ); ?>">
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-book-series"><?php _e( 'Series', 'book-database' ); ?></label>
-			<input type="text" id="bookdb-book-series" name="series" value="<?php echo esc_attr( wp_strip_all_tags( $vars['series_name'] ) ); ?>">
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-book-rating"><?php _e( 'Rating', 'book-database' ); ?></label>
-			<select id="bookdb-book-rating" name="rating">
-				<option value="any"<?php selected( $current_rating, 'any' ) ?>><?php _e( 'Any', 'book-database' ); ?></option>
-				<?php foreach ( bdb_get_available_ratings() as $value => $name ) : ?>
-					<option value="<?php echo esc_attr( $value ); ?>" <?php selected( $current_rating, $value ); ?>><?php echo esc_html( $name ); ?></option>
-				<?php endforeach; ?>
-			</select>
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-genre"><?php _e( 'Genre', 'book-database' ); ?></label>
-			<?php echo book_database()->html->term_dropdown( 'genre', array(
-				'id'       => 'bookdb-genre',
-				'selected' => isset( $vars['terms']['genre'] ) ? absint( $vars['terms']['genre'] ) : 0
-			) ); ?>
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-publisher"><?php _e( 'Publisher', 'book-database' ); ?></label>
-			<?php echo book_database()->html->term_dropdown( 'publisher', array(
-				'id'       => 'bookdb-publisher',
-				'selected' => isset( $vars['terms']['publisher'] ) ? absint( $vars['terms']['publisher'] ) : 0
-			) ); ?>
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-review-year"><?php _e( 'Review Year', 'book-database' ); ?></label>
-			<?php echo book_database()->html->select( array(
-				'id'               => 'bookdb-review-year',
-				'name'             => 'review_year',
-				'options'          => $review_years,
-				'show_option_all'  => esc_html__( 'Any', 'book-database' ),
-				'show_option_none' => false,
-				'selected'         => isset( $vars['year'] ) ? $vars['year'] : 'any'
-			) ); ?>
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-orderby"><?php _e( 'Order by', 'book-database' ); ?></label>
-			<select id="bookdb-orderby" name="orderby">
-				<option value="date"<?php selected( $vars['orderby'], 'date' ) ?>><?php _e( 'Review Date', 'book-database' ); ?></option>
-				<option value="title"<?php selected( $vars['orderby'], 'title' ) ?>><?php _e( 'Book Title', 'book-database' ); ?></option>
-				<option value="author"<?php selected( $vars['orderby'], 'author' ) ?>><?php _e( 'Author Name', 'book-database' ); ?></option>
-				<option value="rating"<?php selected( $vars['orderby'], 'rating' ) ?>><?php _e( 'Rating', 'book-database' ); ?></option>
-				<option value="pub_date"<?php selected( $vars['orderby'], 'pub_date' ) ?>><?php _e( 'Publication Date', 'book-database' ); ?></option>
-				<option value="pages"<?php selected( $vars['orderby'], 'pages' ) ?>><?php _e( 'Number of Pages', 'book-database' ); ?></option>
-			</select>
-		</p>
-
-		<p class="bookdb-filter-option">
-			<label for="bookdb-order"><?php _e( 'Order', 'book-database' ); ?></label>
-			<select id="bookdb-order" name="order">
-				<option value="ASC"<?php selected( $vars['order'], 'ASC' ) ?>><?php _e( 'ASC (1, 2, 3; a, b, c)', 'book-database' ); ?></option>
-				<option value="DESC"<?php selected( $vars['order'], 'DESC' ) ?>><?php _e( 'DESC (3, 2, 1; c, b, a)', 'book-database' ); ?></option>
-			</select>
-		</p>
-
-		<div class="bookdb-filter-actions">
-			<button type="submit"><?php _e( 'Filter', 'book-database' ); ?></button>
-			<a href="<?php echo esc_url( get_permalink() ); ?>" class="bookdb-reset-search-filters"><?php _e( 'Clear filters &times;', 'book-database' ); ?></a>
-		</div>
+		<?php do_action( 'book-database/shortcodes/book-reviews/filter-form', $vars, $query, $atts, $content ); ?>
 	</form>
 	<?php
 
@@ -227,3 +134,281 @@ function bdb_book_reviews_shortcode( $atts, $content = '' ) {
 }
 
 add_shortcode( 'book-reviews', 'bdb_book_reviews_shortcode' );
+
+/**
+ * Filter: Title
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_title( $vars, $query, $atts, $content ) {
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-book-title"><?php _e( 'Book Title', 'book-database' ); ?></label>
+		<input type="text" id="bookdb-book-title" name="title" value="<?php echo esc_attr( wp_strip_all_tags( $vars['book_title'] ) ); ?>">
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_title', 10, 4 );
+
+/**
+ * Filter: Author
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_author( $vars, $query, $atts, $content ) {
+	$author_name = $vars['author_name'];
+
+	if ( isset( $vars['terms']['author'] ) ) {
+		$author = bdb_get_term( array(
+			'term_id' => absint( $vars['terms']['author'] ),
+			'fields'  => 'names'
+		) );
+
+		if ( $author ) {
+			$author_name = $author;
+		}
+	}
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-book-author"><?php _e( 'Author', 'book-database' ); ?></label>
+		<input type="text" id="bookdb-book-author" name="author" value="<?php echo esc_attr( wp_strip_all_tags( $author_name ) ); ?>">
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_author', 10, 4 );
+
+/**
+ * Filter: Series
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_series( $vars, $query, $atts, $content ) {
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-book-series"><?php _e( 'Series', 'book-database' ); ?></label>
+		<input type="text" id="bookdb-book-series" name="series" value="<?php echo esc_attr( wp_strip_all_tags( $vars['series_name'] ) ); ?>">
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_series', 10, 4 );
+
+/**
+ * Filter: Rating
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_rating( $vars, $query, $atts, $content ) {
+	$current_rating = $vars['rating'] ? $vars['rating'] : 'any';
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-book-rating"><?php _e( 'Rating', 'book-database' ); ?></label>
+		<select id="bookdb-book-rating" name="rating">
+			<option value="any"<?php selected( $current_rating, 'any' ) ?>><?php _e( 'Any', 'book-database' ); ?></option>
+			<?php foreach ( bdb_get_available_ratings() as $value => $name ) : ?>
+				<option value="<?php echo esc_attr( $value ); ?>" <?php selected( $current_rating, $value ); ?>><?php echo esc_html( $name ); ?></option>
+			<?php endforeach; ?>
+		</select>
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_rating', 10, 4 );
+
+/**
+ * Filter: Genre
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_genre( $vars, $query, $atts, $content ) {
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-genre"><?php _e( 'Genre', 'book-database' ); ?></label>
+		<?php echo book_database()->html->term_dropdown( 'genre', array(
+			'id'       => 'bookdb-genre',
+			'selected' => isset( $vars['terms']['genre'] ) ? absint( $vars['terms']['genre'] ) : 0
+		) ); ?>
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_genre', 10, 4 );
+
+/**
+ * Filter: Publisher
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_publisher( $vars, $query, $atts, $content ) {
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-publisher"><?php _e( 'Publisher', 'book-database' ); ?></label>
+		<?php echo book_database()->html->term_dropdown( 'publisher', array(
+			'id'       => 'bookdb-publisher',
+			'selected' => isset( $vars['terms']['publisher'] ) ? absint( $vars['terms']['publisher'] ) : 0
+		) ); ?>
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_publisher', 10, 4 );
+
+/**
+ * Filter: Review Year
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_review_year( $vars, $query, $atts, $content ) {
+	$review_years = bdb_get_review_years();
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-review-year"><?php _e( 'Review Year', 'book-database' ); ?></label>
+		<?php echo book_database()->html->select( array(
+			'id'               => 'bookdb-review-year',
+			'name'             => 'review_year',
+			'options'          => $review_years,
+			'show_option_all'  => esc_html__( 'Any', 'book-database' ),
+			'show_option_none' => false,
+			'selected'         => isset( $vars['year'] ) ? $vars['year'] : 'any'
+		) ); ?>
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_review_year', 10, 4 );
+
+/**
+ * Filter: Order By
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_orderby( $vars, $query, $atts, $content ) {
+	$allowed_orderby = array(
+		'date'     => esc_html__( 'Review Date', 'book-database' ),
+		'title'    => esc_html__( 'Book Title', 'book-database' ),
+		'author'   => esc_html__( 'Author Name', 'book-database' ),
+		'rating'   => esc_html__( 'Rating', 'book-database' ),
+		'pub_date' => esc_html__( 'Publication Date', 'book-database' ),
+		'pages'    => esc_html__( 'Number of Pages', 'book-database' )
+	);
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-orderby"><?php _e( 'Order by', 'book-database' ); ?></label>
+		<?php echo book_database()->html->select( array(
+			'id'               => 'bookdb-orderby',
+			'name'             => 'orderby',
+			'selected'         => sanitize_text_field( $vars['orderby'] ),
+			'options'          => apply_filters( 'book-database/shortcodes/book-reviews/orderby-options', $allowed_orderby ),
+			'show_option_all'  => false,
+			'show_option_none' => false
+		) ); ?>
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_orderby', 10, 4 );
+
+/**
+ * Filter: Order
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_order( $vars, $query, $atts, $content ) {
+	$allowed_order = array(
+		'ASC'  => esc_html__( 'ASC (1, 2, 3; a, b, c)', 'book-database' ),
+		'DESC' => esc_html__( 'DESC (3, 2, 1; c, b, a)', 'book-database' )
+	);
+	?>
+	<p class="bookdb-filter-option">
+		<label for="bookdb-order"><?php _e( 'Order', 'book-database' ); ?></label>
+		<?php echo book_database()->html->select( array(
+			'id'               => 'bookdb-order',
+			'name'             => 'order',
+			'selected'         => sanitize_text_field( $vars['order'] ),
+			'options'          => apply_filters( 'book-database/shortcodes/book-reviews/order-options', $allowed_order ),
+			'show_option_all'  => false,
+			'show_option_none' => false
+		) ); ?>
+	</p>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_order', 10, 4 );
+
+/**
+ * Filter: Submit Button
+ *
+ * @param array            $vars    Query vars.
+ * @param BDB_Review_Query $query   Review query.
+ * @param array            $atts    Shortcode attributes.
+ * @param string           $content Shortcode content.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function bdb_reviews_filter_form_submit( $vars, $query, $atts, $content ) {
+	?>
+	<div class="bookdb-filter-actions">
+		<button type="submit"><?php _e( 'Filter', 'book-database' ); ?></button>
+		<a href="<?php echo esc_url( get_permalink() ); ?>" class="bookdb-reset-search-filters"><?php _e( 'Clear filters &times;', 'book-database' ); ?></a>
+	</div>
+	<?php
+}
+
+add_action( 'book-database/shortcodes/book-reviews/filter-form', 'bdb_reviews_filter_form_submit', 200, 4 );

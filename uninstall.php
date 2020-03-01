5233f4739c08ec935821e3fd7c440b96d85d7a21
Ashley Gibson <ashley@nosegraze.com>
2020-03-01 09:16:36 +0000

    Use get_tables() method to get all registered tables #218

--- uninstall.php
+++ uninstall.php
@@ -4,10 +4,11 @@
  *
  * Delete the following data when the plugin is uninstalled:
  *      - All options.
- *      - Drop custom tables.
+ *      - Custom tables.
+ *      - Custom capabilities.
  *
  * @package   book-database
- * @copyright Copyright (c) 2017, Ashley Gibson
+ * @copyright Copyright (c) 2020, Ashley Gibson
  * @license   GPL2+
  * @since     1.0
  */
@@ -33,29 +34,7 @@ global $wpdb;
 $wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'bdb\_%'" );
 
 // Drop tables
-$tables = array(
-	'authors',
-	'book_author_relationships',
-	'book_taxonomies',
-	'book_term_relationships',
-	'book_terms',
-	'books',
-	'book_meta',
-	'owned_editions',
-	'reading_log',
-	'reviewmeta',
-	'reviews',
-	'review_meta',
-	'series'
-);
-
-foreach ( $tables as $table_key ) {
-	$table = book_database()->get_table( $table_key );
-
-	if ( ! $table ) {
-		continue;
-	}
-
+foreach ( book_database()->get_tables() as $table ) {
 	if ( $table->exists() ) {
 		$table->uninstall();
 	}

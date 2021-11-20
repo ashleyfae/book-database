a6da6bf1706b2e76d7a4e9fc4cf5f5f9edad6e28
Ashley Gibson <ashley@nosegraze.com>
2021-11-20 14:25:36 +0000

    Set up container, service providers #248

---
+++ src/Plugin.php
@@ -0,0 +1,300 @@
+<?php
+/**
+ * Plugin.php
+ *
+ * @package   book-database
+ * @copyright Copyright (c) 2021, Ashley Gibson
+ * @license   GPL2+
+ * @since     1.3
+ */
+
+namespace Book_Database;
+
+use Book_Database\Container\Container;
+use Book_Database\Container\Exceptions\BindingResolutionException;
+use Book_Database\ServiceProviders\LegacyServiceProvider;
+use Book_Database\ServiceProviders\ServiceProvider;
+
+final class Plugin
+{
+
+    /**
+     * @var Plugin
+     */
+    private static $instance;
+
+    /**
+     * @var Container
+     * @since 1.3
+     */
+    private $container;
+
+    /**
+     * @var string[]
+     * @since 1.3
+     */
+    private $serviceProviders = [
+        LegacyServiceProvider::class,
+    ];
+
+    /**
+     * @var bool
+     * @since 1.3
+     */
+    private $serviceProvidersLoaded = false;
+
+    /**
+     * Array of custom table objects
+     *
+     * @var array
+     */
+    private $tables = array();
+
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
+
+    /**
+     * Plugin instance.
+     *
+     * @return Plugin Instance of Plugin class
+     */
+    public static function instance(): Plugin
+    {
+        // Return if already instantiated
+        if (self::isInstantiated()) {
+            return self::$instance;
+        }
+
+        // Set up the singleton.
+        self::$instance = new Plugin();
+
+        // Bootstrap
+        self::$instance->setup_application();
+
+        add_action('admin_init', array(self::$instance, 'install'), 11);
+
+        return self::$instance;
+    }
+
+    /**
+     * Magic methods are passed to the service container.
+     *
+     * @since 1.3
+     *
+     * @param  string  $name
+     * @param  mixed  $arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->container, $name], $arguments);
+    }
+
+    /**
+     * Properties are passed to the service container.
+     *
+     * @since 1.3
+     *
+     * @param  string  $propertyName
+     *
+     * @return mixed|object
+     * @throws BindingResolutionException
+     */
+    public function __get($propertyName)
+    {
+        return $this->container->get($propertyName);
+    }
+
+    /**
+     * Whether the main class has been instantiated or not.
+     *
+     * @return bool
+     */
+    private static function isInstantiated(): bool
+    {
+        return ! empty(self::$instance) && self::$instance instanceof Plugin;
+    }
+
+    /**
+     * Set up custom database tables
+     */
+    private function setup_application()
+    {
+        self::$instance->loadServiceProviders();
+
+        self::$instance->tables = array(
+            'authors'                   => new Authors_Table(),
+            'book_author_relationships' => new Book_Author_Relationships_Table(),
+            'book_links'                => new Book_Links_Table(),
+            'book_taxonomies'           => new Book_Taxonomies_Table(),
+            'book_term_relationships'   => new Book_Term_Relationships_Table(),
+            'book_terms'                => new Book_Terms_Table(),
+            'books'                     => new Books_Table(),
+            'book_meta'                 => new Book_Meta_Table(),
+            'editions'                  => new Editions_Table(),
+            'reading_log'               => new Reading_Logs_Table(),
+            'retailers'                 => new Retailers_Table(),
+            'reviews'                   => new Reviews_Table(),
+            'review_meta'               => new Review_Meta_Table(),
+            'series'                    => new Series_Table(),
+        );
+
+    }
+
+    private function loadServiceProviders(): void
+    {
+        if ($this->serviceProvidersLoaded) {
+            return;
+        }
+
+        $providers = [];
+
+        foreach ($this->serviceProviders as $serviceProvider) {
+            if (! is_subclass_of($serviceProvider, ServiceProvider::class)) {
+                throw new \InvalidArgumentException(sprintf(
+                    '%s class must implement the %s interface.',
+                    $serviceProvider,
+                    ServiceProvider::class
+                ));
+            }
+
+            /** @var ServiceProvider $serviceProvider */
+            $serviceProvider = new $serviceProvider;
+            $serviceProvider->register();
+            $providers[] = $serviceProvider;
+        }
+
+        foreach ($providers as $serviceProvider) {
+            $serviceProvider->boot();
+        }
+
+        $this->serviceProvidersLoaded = true;
+    }
+
+    /**
+     * Get a table object by its key
+     *
+     * @param  string  $table_key  Table key.  One of:
+     *                          'authors',
+     *                          'book_author_relationships',
+     *                          'book_links',
+     *                          'book_taxonomies',
+     *                          'book_term_relationships',
+     *                          'book_terms'
+     *                          'books'
+     *                          'book_meta',
+     *                          'editions'
+     *                          'reading_log'
+     *                          'retailers',
+     *                          'reviews',
+     *                          'review_meta',
+     *                          'series'
+     *
+     * @return BerlinDB\Database\Table|false
+     */
+    public function get_table(string $table_key)
+    {
+        return array_key_exists($table_key, self::$instance->tables) ? self::$instance->tables[$table_key] : false;
+    }
+
+    /**
+     * Returns an array of all registered tables
+     *
+     * @return BerlinDB\Database\Table[]
+     */
+    public function get_tables(): array
+    {
+        return self::$instance->tables;
+    }
+
+    /**
+     * Get the HTML helper class
+     *
+     * @return HTML
+     */
+    public function get_html(): HTML
+    {
+        return $this->html;
+    }
+
+    /**
+     * Run installation
+     *
+     *      - Install default taxonomies.
+     *      - Add rewrite tags/rules and flush
+     */
+    public function install(): void
+    {
+        if (! get_option('bdb_run_activation')) {
+            return;
+        }
+
+        /**
+         * Add default taxonomies.
+         */
+        if (! $this->get_table('book_taxonomies')->exists()) {
+            $this->get_table('book_taxonomies')->install();
+        }
+
+        $default_taxonomies = array(
+            'publisher' => array(
+                'slug'   => 'publisher',
+                'name'   => esc_html__('Publisher', 'book-database'),
+                'format' => 'text' // text, checkbox
+            ),
+            'genre'     => array(
+                'slug'   => 'genre',
+                'name'   => esc_html__('Genre', 'book-database'),
+                'format' => 'text'
+            ),
+            'source'    => array(
+                'slug'   => 'source',
+                'name'   => esc_html__('Source', 'book-database'),
+                'format' => 'checkbox'
+            )
+        );
+
+        foreach ($default_taxonomies as $taxonomy) {
+            if (get_book_taxonomy_by('slug', $taxonomy['slug'])) {
+                continue;
+            }
+
+            try {
+                add_book_taxonomy($taxonomy);
+            } catch (Exception $e) {
+
+            }
+        }
+
+        /**
+         * Add rewrite tags/rules and flush
+         */
+        add_rewrite_tags();
+        add_rewrite_rules();
+        flush_rewrite_rules(true);
+
+        /**
+         * Add capabilities
+         */
+        $role = get_role('administrator');
+
+        foreach (get_book_capabilities() as $capability) {
+            $role->add_cap($capability, true);
+        }
+
+        /**
+         * Set version number
+         */
+        update_option('bdb_version', BDB_VERSION);
+
+        if (! get_option('bdb_install_date')) {
+            update_option('bdb_install_date', date('Y-m-d H:i:s'), false);
+        }
+
+        delete_option('bdb_run_activation');
+    }
+
+}
---
+++ src/ServiceProviders/LegacyServiceProvider.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * LegacyServiceProvider.php
+ *
+ * @package   book-database
+ * @copyright Copyright (c) 2021, Ashley Gibson
+ * @license   GPL2+
+ */
+
+namespace Book_Database\ServiceProviders;
+
+use Book_Database\HTML;
+use Book_Database\Plugin;
+use Book_Database\REST_API;
+use function Book_Database\book_database;
+
+class LegacyServiceProvider implements ServiceProvider
+{
+
+    public function register(): void
+    {
+        $this->includeLegacyFiles();
+
+        if (is_admin() || (defined('WP_CLI') && WP_CLI)) {
+            $this->includeAdminFiles();
+        } else {
+            $this->includeFrontendFiles();
+        }
+
+        $this->registerClassAliases();
+        $this->bindClasses();
+    }
+
+    public function boot(): void
+    {
+
+    }
+
+    private function includeLegacyFiles(): void
+    {
+        // Database engine
+        require_once BDB_DIR.'includes/database/engine/base.php';
+        require_once BDB_DIR.'includes/database/engine/table.php';
+        require_once BDB_DIR.'includes/database/engine/query.php';
+        require_once BDB_DIR.'includes/database/engine/column.php';
+        require_once BDB_DIR.'includes/database/engine/row.php';
+        require_once BDB_DIR.'includes/database/engine/schema.php';
+        require_once BDB_DIR.'includes/database/engine/compare.php';
+        require_once BDB_DIR.'includes/database/engine/date.php';
+        require_once BDB_DIR.'includes/database/engine/series.php';
+        require_once BDB_DIR.'includes/database/engine/tax.php';
+        require_once BDB_DIR.'includes/database/engine/author.php';
+        require_once BDB_DIR.'includes/database/engine/join.php';
+        require_once BDB_DIR.'includes/database/engine/book.php';
+        require_once BDB_DIR.'includes/database/engine/edition.php';
+        require_once BDB_DIR.'includes/database/engine/reading-log.php';
+        require_once BDB_DIR.'includes/database/engine/class-where-clause.php';
+        require_once BDB_DIR.'includes/database/sanitization.php';
+
+        // Database - authors
+        require_once BDB_DIR.'includes/database/authors/class-authors-table.php';
+        require_once BDB_DIR.'includes/database/authors/class-authors-schema.php';
+        require_once BDB_DIR.'includes/database/authors/class-authors-query.php';
+
+        // Database - book_author_relationships
+        require_once BDB_DIR.'includes/database/book-author-relationships/class-book-author-relationships-table.php';
+        require_once BDB_DIR.'includes/database/book-author-relationships/class-book-author-relationships-schema.php';
+        require_once BDB_DIR.'includes/database/book-author-relationships/class-book-author-relationships-query.php';
+
+        // Database - books
+        require_once BDB_DIR.'includes/database/books/class-books-table.php';
+        require_once BDB_DIR.'includes/database/books/class-books-schema.php';
+        require_once BDB_DIR.'includes/database/books/class-books-query.php';
+        require_once BDB_DIR.'includes/database/books/class-book-meta-table.php';
+
+        // Database - book_links
+        require_once BDB_DIR.'includes/database/book-links/class-book-links-table.php';
+        require_once BDB_DIR.'includes/database/book-links/class-book-links-schema.php';
+        require_once BDB_DIR.'includes/database/book-links/class-book-links-query.php';
+
+        // Database - book_taxonomies
+        require_once BDB_DIR.'includes/database/book-taxonomies/class-book-taxonomies-table.php';
+        require_once BDB_DIR.'includes/database/book-taxonomies/class-book-taxonomies-schema.php';
+        require_once BDB_DIR.'includes/database/book-taxonomies/class-book-taxonomies-query.php';
+
+        // Database - book_term_relationships
+        require_once BDB_DIR.'includes/database/book-term-relationships/class-book-term-relationships-table.php';
+        require_once BDB_DIR.'includes/database/book-term-relationships/class-book-term-relationships-schema.php';
+        require_once BDB_DIR.'includes/database/book-term-relationships/class-book-term-relationships-query.php';
+
+        // Database - book_terms
+        require_once BDB_DIR.'includes/database/book-terms/class-book-terms-table.php';
+        require_once BDB_DIR.'includes/database/book-terms/class-book-terms-schema.php';
+        require_once BDB_DIR.'includes/database/book-terms/class-book-terms-query.php';
+
+        // Database - owned_editions
+        require_once BDB_DIR.'includes/database/editions/class-editions-table.php';
+        require_once BDB_DIR.'includes/database/editions/class-editions-schema.php';
+        require_once BDB_DIR.'includes/database/editions/class-editions-query.php';
+
+        // Database - reading_log
+        require_once BDB_DIR.'includes/database/reading-logs/class-reading-logs-table.php';
+        require_once BDB_DIR.'includes/database/reading-logs/class-reading-logs-schema.php';
+        require_once BDB_DIR.'includes/database/reading-logs/class-reading-logs-query.php';
+
+        // Database - retailers
+        require_once BDB_DIR.'includes/database/retailers/class-retailers-table.php';
+        require_once BDB_DIR.'includes/database/retailers/class-retailers-schema.php';
+        require_once BDB_DIR.'includes/database/retailers/class-retailers-query.php';
+
+        // Database - reviews
+        require_once BDB_DIR.'includes/database/reviews/class-reviews-table.php';
+        require_once BDB_DIR.'includes/database/reviews/class-reviews-schema.php';
+        require_once BDB_DIR.'includes/database/reviews/class-reviews-query.php';
+        require_once BDB_DIR.'includes/database/reviews/class-review-meta-table.php';
+
+        // Database - series
+        require_once BDB_DIR.'includes/database/series/class-series-table.php';
+        require_once BDB_DIR.'includes/database/series/class-series-schema.php';
+        require_once BDB_DIR.'includes/database/series/class-series-query.php';
+
+        // Analytics
+        require_once BDB_DIR.'includes/analytics/analytics-functions.php';
+
+        // Authors
+        require_once BDB_DIR.'includes/authors/author-functions.php';
+
+        // Blocks
+        require_once BDB_DIR.'includes/blocks.php';
+
+        // Book Author Relationships
+        require_once BDB_DIR.'includes/book-author-relationships/book-author-relationship-actions.php';
+        require_once BDB_DIR.'includes/book-author-relationships/book-author-relationship-functions.php';
+
+        // Books
+        require_once BDB_DIR.'includes/books/book-functions.php';
+        require_once BDB_DIR.'includes/books/book-layout-functions.php';
+        require_once BDB_DIR.'includes/books/book-meta.php';
+
+        // Book Links
+        require_once BDB_DIR.'includes/book-links/book-link-functions.php';
+
+        // Book Taxonomies
+        require_once BDB_DIR.'includes/book-taxonomies/book-taxonomy-functions.php';
+
+        // Book Term Relationships
+        require_once BDB_DIR.'includes/book-term-relationships/book-term-relationship-actions.php';
+        require_once BDB_DIR.'includes/book-term-relationships/book-term-relationship-functions.php';
+
+        // Book Terms
+        require_once BDB_DIR.'includes/book-terms/book-term-functions.php';
+
+        // Editions
+        require_once BDB_DIR.'includes/editions/edition-functions.php';
+
+        // Ratings
+        require_once BDB_DIR.'includes/ratings/rating-functions.php';
+
+        // Reading Logs
+        require_once BDB_DIR.'includes/reading-logs/reading-log-functions.php';
+
+        // Retailers
+        require_once BDB_DIR.'includes/retailers/retailer-functions.php';
+
+        // Reviews
+        require_once BDB_DIR.'includes/reviews/review-actions.php';
+        require_once BDB_DIR.'includes/reviews/review-functions.php';
+        require_once BDB_DIR.'includes/reviews/review-meta.php';
+
+        // Series
+        require_once BDB_DIR.'includes/series/series-functions.php';
+
+        // Misc.
+        require_once BDB_DIR.'includes/capabilities.php';
+        require_once BDB_DIR.'includes/misc-functions.php';
+        require_once BDB_DIR.'includes/rewrites.php';
+        require_once BDB_DIR.'includes/shortcodes.php';
+        require_once BDB_DIR.'includes/template-functions.php';
+    }
+
+    private function includeAdminFiles(): void
+    {
+        require_once BDB_DIR.'includes/admin/abstract-class-list-table.php';
+        require_once BDB_DIR.'includes/admin/admin-assets.php';
+        require_once BDB_DIR.'includes/admin/admin-bar.php';
+        require_once BDB_DIR.'includes/admin/admin-notices.php';
+        require_once BDB_DIR.'includes/admin/admin-pages.php';
+
+        // Analytics
+        require_once BDB_DIR.'includes/admin/analytics/analytics-page.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/overview.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/library.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/reading.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/ratings.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/editions.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/reviews.php';
+        require_once BDB_DIR.'includes/admin/analytics/tabs/terms.php';
+
+        // Authors
+        require_once BDB_DIR.'includes/admin/authors/author-actions.php';
+        require_once BDB_DIR.'includes/admin/authors/author-functions.php';
+        require_once BDB_DIR.'includes/admin/authors/authors-page.php';
+
+        // Book Terms
+        require_once BDB_DIR.'includes/admin/book-terms/book-term-actions.php';
+        require_once BDB_DIR.'includes/admin/book-terms/book-term-functions.php';
+        require_once BDB_DIR.'includes/admin/book-terms/book-terms-page.php';
+
+        // Books
+        require_once BDB_DIR.'includes/admin/books/book-actions.php';
+        require_once BDB_DIR.'includes/admin/books/book-functions.php';
+        require_once BDB_DIR.'includes/admin/books/books-page.php';
+        require_once BDB_DIR.'includes/admin/books/edit-book-fields.php';
+
+        // Dashboard
+        require_once BDB_DIR.'includes/admin/dashboard/widgets.php';
+
+        // Editions
+        require_once BDB_DIR.'includes/admin/editions/edition-actions.php';
+
+        // Licensing
+        require_once BDB_DIR.'includes/class-edd-sl-plugin-updater.php';
+        require_once BDB_DIR.'includes/admin/licensing/class-license-key.php';
+        require_once BDB_DIR.'includes/admin/licensing/license-actions.php';
+
+        // Posts
+        require_once BDB_DIR.'includes/admin/posts/post-actions.php';
+
+        // Reading Logs
+        require_once BDB_DIR.'includes/admin/reading-logs/reading-log-actions.php';
+
+        // Reviews
+        require_once BDB_DIR.'includes/admin/reviews/review-actions.php';
+        require_once BDB_DIR.'includes/admin/reviews/review-fields.php';
+        require_once BDB_DIR.'includes/admin/reviews/review-functions.php';
+        require_once BDB_DIR.'includes/admin/reviews/reviews-page.php';
+
+        // Series
+        require_once BDB_DIR.'includes/admin/series/series-actions.php';
+        require_once BDB_DIR.'includes/admin/series/series-functions.php';
+        require_once BDB_DIR.'includes/admin/series/series-page.php';
+
+        // Settings
+        require_once BDB_DIR.'includes/admin/settings/book-layout-functions.php';
+        require_once BDB_DIR.'includes/admin/settings/register-settings.php';
+        require_once BDB_DIR.'includes/admin/settings/display-settings.php';
+    }
+
+    private function includeFrontendFiles(): void
+    {
+        require_once BDB_DIR.'includes/assets.php';
+    }
+
+    private function registerClassAliases(): void
+    {
+        class_alias(Plugin::class, 'Book_Database\\Book_Database');
+    }
+
+    private function bindClasses(): void
+    {
+        book_database()->bind(REST_API::class);
+        book_database()->alias(REST_API::class, 'rest_api');
+
+        book_database()->bind(HTML::class);
+        book_database()->alias(HTML::class, 'html');
+    }
+}

5921266caf92d918d6c7966fa618c28a9b959aee
Ashley Gibson <ashley@nosegraze.com>
2021-12-11 11:13:57 +0000

    Fix unit tests for latest WP #257

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,6 +16,8 @@ $_SERVER['SERVER_NAME']     = '';
 
 define( 'BDB_DOING_TESTS', true );
 
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
